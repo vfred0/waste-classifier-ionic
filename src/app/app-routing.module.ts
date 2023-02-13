4adb6d1ee89fb2b84857d4f19d14501e9cf9ebdc
Víctor Arreaga <arreagav17@gmail.com>
2023-02-12 19:03:43 -0500

    app routes
    Took 1 minute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,50 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
 
+const routes: Routes = [
+  {
+    path: '',
+    component: NavBarComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./routes/home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'scan',
+        loadChildren: () => import('./routes/scan/scan.module').then(m => m.ScanPageModule)
+      },
+    ]
+  },
+
+
+];
+
+/*
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./routes/home/home.module').then( m => m.HomePageModule)
+  },
 ];
+*/
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
